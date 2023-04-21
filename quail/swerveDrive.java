f5279c77195dc728e35b4c748e8405492dcc8c44
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2023-04-21 11:17:17 -0700

    added calculateMoveAngles

--- quail/swerveDrive.java
+++ quail/swerveDrive.java
@@ -1,4 +1,24 @@
 package quail;
+import java.util.List;
 
 public class swerveDrive {
+    public final List<swerveModuleBase> swerveModules;
+    public swerveDrive(List<swerveModuleBase> swerveModules) {
+        this.swerveModules = swerveModules;
+    }
+    /**
+     * @param moveVector the vector to move in
+     * @param rotationSpeed speed of rotation
+     * @param gyroOffset the gyro rotation in radians
+     */
+    public Vec2d[] calculateMoveAngles(Vec2d moveVector, double rotationSpeed, double gyroOffset) {
+        moveVector = moveVector.rotate(gyroOffset, false);
+         // create a list of four vec2d objects and iterate over them with a for loop (not foreach)
+        Vec2d[] moduleVectors = new Vec2d[this.swerveModules.size()];
+        for (int i = 0; i < 4; i++) {
+            swerveModuleBase module = swerveModules.get(i);
+            moduleVectors[i] = moveVector.add(module.steeringVector.scale(rotationSpeed));
+        }
+        return moduleVectors;
+    }
 }
