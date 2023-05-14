0e64a8db11e19ac12de2ef4935c5386a5a701339
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2023-05-13 20:02:25 -0700

    fix the deltaAngle function

--- quail/src/main/java/com/mineinjava/quail/util/util.java
+++ quail/src/main/java/com/mineinjava/quail/util/util.java
@@ -9,6 +9,7 @@ import com.mineinjava.quail.swerveDrive;
 public class util {
     /** Calculates the smallest angle between two angles
      * Useful for determining how far and in which direction to rotate anything
+     * angle returned is in radians, counterclockwise from angle 1 to angle 2
      * @param angle1
      * @param angle2
      * @return
@@ -17,12 +18,13 @@ public class util {
         // calculate the smallest angle to rotate between the current angle and the target angle
         double deltaAngle = angle2 % (Math.PI/2) - angle1 % (Math.PI/2);
         if (deltaAngle > Math.PI) {
-            deltaAngle -= 2 * Math.PI;
-        } else if (deltaAngle < -Math.PI) {
             deltaAngle += 2 * Math.PI;
+        } else if (deltaAngle < -Math.PI) {
+            deltaAngle -= 2 * Math.PI;
         }
         return deltaAngle;
     }
+
 
     /** clamps a value between a min and a max
      *
