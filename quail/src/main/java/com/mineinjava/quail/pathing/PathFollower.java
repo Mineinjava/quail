b02d15dc10bbccc54aa8f726c45b76188d7c754f
astr0clad <colin.montigel@gmail.com>
2023-11-21 16:20:18 -0700

    define deltaAngle

--- quail/src/main/java/com/mineinjava/quail/pathing/PathFollower.java
+++ quail/src/main/java/com/mineinjava/quail/pathing/PathFollower.java
@@ -57,10 +57,11 @@ public class PathFollower {
     public RobotMovement calculateNextDriveMovement() {
         // calculate the next movement to follow the path
         Pose2d currentPose = this.localizer.getPoseEstimate();
+        double deltaAngle = 0.0;
         if (currentPose == null) {
             throw new NullPointerException("localizer.getPoseEstimate is null");
         } else {
-            double deltaAngle = Util.deltaAngle(currentPose.heading, this.path.getCurrentPoint().heading); // this may or may not work
+            deltaAngle = Util.deltaAngle(currentPose.heading, this.path.getCurrentPoint().heading); // this may or may not work
         }
 
         double turnSpeed = turnController.getOutput(0, deltaAngle);
