14fa8d7b0fd3476f581fac40771a2cf3571ac041
astr0clad <colin.montigel@gmail.com>
2023-11-22 11:21:49 -0700

    more debugging

--- quail/src/main/java/com/mineinjava/quail/pathing/PathFollower.java
+++ quail/src/main/java/com/mineinjava/quail/pathing/PathFollower.java
@@ -59,7 +59,7 @@ public class PathFollower {
         // calculate the next movement to follow the path
         Pose2d currentPose = this.localizer.getPoseEstimate();
         this.currentPose = currentPose;
-        if (currentPose == null) {
+        if (currentPose.heading == 0.0) {
             throw new NullPointerException("localizer.getPoseEstimate is null");
         }
         double deltaAngle = Util.deltaAngle(currentPose.heading, this.path.getCurrentPoint().heading); // this may or may not work
