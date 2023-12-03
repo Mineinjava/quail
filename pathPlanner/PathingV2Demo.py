6978dbe8f9ed967cd812db49e1526faed4a3cbd3
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2023-12-02 18:16:23 -0800

    add circle detection and progressive drawing

--- pathPlanner/PathingV2Demo.py
+++ pathPlanner/PathingV2Demo.py
@@ -2,19 +2,19 @@ import random
 
 import matplotlib.pyplot as plt
 
-LOOPTIME = 0.150  # seconds
+LOOPTIME = 0.250  # seconds
 
-MAXVELOCITY = 2.25 #
-MAXACCELERATION = 2.5  # m/s^2
+MAXVELOCITY = 2.25  #
+MAXACCELERATION = 2  # m/s^2
 MAXANGULARVELOCITY = 1.0  # rad/s
 MAXANGULARACCELERATION = 1.0  # rad/s^2
 
-CruiseVelocity = 2
+CruiseVelocity = 1.3
 
-PRECISION_RADIUS = 0.1
-LOOPTIME_DEVIATION = 0  # seconds
+PRECISION_RADIUS = 0.0254
+LOOPTIME_DEVIATION = 0.00  # seconds
 
-SLOW_DOWN_RADIUS = 0.5
+SLOW_DOWN_RADIUS = 1
 
 
 class Pose2d:
@@ -51,22 +51,29 @@ class Pose2d:
             self.y) + ", theta: " + str(self.theta)
 
 
+# https://stackoverflow.com/a/1084899/13224997
 def circle_intersect_line(pose, x1, y1, x2, y2):
-    # subtract pose from each of the points -- find a better way to do this.
-    x1 -= pose.x
-    y1 -= pose.y
-    x2 -= pose.x
-    y2 -= pose.y
+    d = (x2 - x1, y2 - y1)
+    f = (x1 - pose.x, y1 - pose.y)
 
-    dy = y2 - y1
-    dx = x2 - x1
-    dr = (dx ** 2 + dy ** 2) ** 0.5
-    D = x1 * y2 - x2 * y1
-    discriminant = PRECISION_RADIUS ** 2 * dr ** 2 - D ** 2
+    a = d[0] ** 2 + d[1] ** 2
+    if a == 0:
+        return False
+    b = 2 * (f[0] * d[0] + f[1] * d[1])
+    c = (f[0] ** 2) + (f[1] ** 2) - (PRECISION_RADIUS ** 2)
+
+    discriminant = (b ** 2) - (4 * a * c)
     if discriminant < 0:
         return False
     else:
-        return True
+        discriminant = discriminant ** 0.5
+        t1 = (-b - discriminant) / (2 * a)
+        t2 = (-b + discriminant) / (2 * a)
+
+        if not (0 <= t1 <= 1 or 0 <= t2 <= 1):
+            return False
+        else:
+            return True
 
 
 class Waypoint(Pose2d):
@@ -76,9 +83,11 @@ class Waypoint(Pose2d):
     def is_hit(self, robotPose, oldRobotPose):
         if self.distance(robotPose) < PRECISION_RADIUS:
             return True
-        else:
-            return False  # circle_intersect_line(self, robotPose.x, robotPose.y,
-            #               oldRobotPose.x, oldRobotPose.y)
+        elif (oldRobotPose.distance(robotPose) > self.distance(
+                robotPose)):
+            print("checkign circle intersect")
+            return circle_intersect_line(self, robotPose.x, robotPose.y,
+                                  oldRobotPose.x, oldRobotPose.y)
 
     def __str__(self):
         return super().__str__()
@@ -89,33 +98,45 @@ class RobotPose(Pose2d):
         super().__init__(x, y, theta)
 
     def __str__(self):
-        return super().__str__() + ", velocity: " + str(self.velocity) \
-            + ", angularVelocity: " + str(self.angularVelocity)
+        return super().__str__() + ", velocity: "\
+            + ", angularVelocity: "
 
 
 if __name__ == "__main__":
     INITIAL_POSE = RobotPose(0, 0, 0)
-    ipoints = [(0, 12), (24, 6), (2, 1), (9, 3), (3, 9)]
+    ipoints = [(0,0), (0, 12), (3, 6), (0, 1), (2, 0), (3, 1)]
     splinex = [point[0] for point in ipoints]
     spliney = [point[1] for point in ipoints]
+    import numpy as np
+
+    h1 = plt.plot([], [], 'rx-')
+    plt.xlim(min(splinex) - 2, max(splinex) + 2)
+    plt.ylim(min(spliney) - 2, max(spliney) + 2)
+    def update_line(hl, new_data):
+        hl.set_xdata(np.append(hl.get_xdata(), new_data[0]))
+        hl.set_ydata(np.append(hl.get_ydata(), new_data[1]))
+        plt.autoscale()
+        plt.draw()
+        plt.pause(0.01)
 
     import cubicSpline
 
-    # splinex, spliney = cubicSpline.interpolate_xy(splinex, spliney, 20)
-    points = list(zip(splinex,spliney))
+    splinex, spliney = cubicSpline.interpolate_xy(splinex, spliney, 20)
+    points = list(zip(splinex, spliney))
     print(points)
-
 
     WAYPOINTS = [Waypoint(point[0], point[1], 0) for point in points]
     print(WAYPOINTS)
     robotPose = INITIAL_POSE
     robotPoseHistory = [robotPose]
 
+
     def loop():
         global LOOPTIME
         drift_looptime = LOOPTIME + random.uniform(-LOOPTIME_DEVIATION,
                                                    LOOPTIME_DEVIATION)
         global robotPose
+        print(robotPose)
         global robotPoseHistory
         if WAYPOINTS[0].is_hit(robotPose, robotPoseHistory[-1]):
             WAYPOINTS.pop(0)
@@ -146,11 +167,16 @@ if __name__ == "__main__":
 
         robotPose.velocity = vel + (accel * LOOPTIME)
         robotPose += robotPose.velocity * drift_looptime
+        # update_line(h1[0], (robotPose.x, robotPose.y))
         return True
 
-
+    import time
+    start = time.perf_counter()
     while loop():
         pass
+    end = time.perf_counter()
+    print("time", end - start, "s, average time", 1000 * (end - start) / len(robotPoseHistory), "ms")
+
 
     x = []
     y = []
@@ -158,7 +184,7 @@ if __name__ == "__main__":
         x.append(pose.x)
         y.append(pose.y)
 
-    print("time", len(robotPoseHistory) * LOOPTIME)
+    print("simulated time: ", len(robotPoseHistory) * LOOPTIME, "s")
 
     plt.plot(x, y, 'ro')
     plt.plot([point[0] for point in ipoints],
