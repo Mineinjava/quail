285c64c9d8de6f21f17b1c182b3d9da2de5808f9
astr0clad <colin.montigel@gmail.com>
2023-12-07 14:50:32 -0700

    null after path completion

--- quail/src/main/java/com/mineinjava/quail/pathing/PathFollower.java
+++ quail/src/main/java/com/mineinjava/quail/pathing/PathFollower.java
@@ -76,6 +76,9 @@ public class PathFollower {
         this.loopTime = (System.currentTimeMillis() - this.lastTime) / 1000.0;
         this.lastTime = System.currentTimeMillis();
 
+        if(this.path.getCurrentPoint() == null){
+            return new RobotMovement(0, new Vec2d(0, 0)); // the path is over
+        }
         double deltaAngle = Util.deltaAngle(currentPose.heading, this.path.getCurrentPoint().heading); // this may or may not work
         if (this.lastRobotPose == null) {
             this.lastRobotPose = currentPose;
