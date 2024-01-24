48d2734c71d78a6413417296908545e929571ed5
astr0clad <colin.montigel@gmail.com>
2024-01-23 22:36:48 -0700

    manual build javadoc

--- quail/build/docs/javadoc/package-search-index.js
+++ quail/build/docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mineinjava.quail"},{"l":"com.mineinjava.quail.localization"},{"l":"com.mineinjava.quail.odometry"},{"l":"com.mineinjava.quail.util"},{"l":"com.mineinjava.quail.util.geometry"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mineinjava.quail"},{"l":"com.mineinjava.quail.localization"},{"l":"com.mineinjava.quail.pathing"},{"l":"com.mineinjava.quail.util"},{"l":"com.mineinjava.quail.util.geometry"}];updateSearchResults();
