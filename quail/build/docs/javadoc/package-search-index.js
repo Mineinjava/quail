5badc696362a84c395033964e8d28934d55ddc5b
astr0clad <colin.montigel@gmail.com>
2024-01-23 22:41:30 -0700

    Clean build folder to force new build?

--- quail/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mineinjava.quail"},{"l":"com.mineinjava.quail.localization"},{"l":"com.mineinjava.quail.pathing"},{"l":"com.mineinjava.quail.util"},{"l":"com.mineinjava.quail.util.geometry"}];updateSearchResults();
