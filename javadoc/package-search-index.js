face17c5b93335f1b73fb6044d8fe311a3f58765
Astr0clad <67985645+Astr0clad@users.noreply.github.com>
2024-01-24 05:39:13 +0000

    Deploying to javadoc from @ Mineinjava/quail@48d2734c71d78a6413417296908545e929571ed5 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mineinjava.quail"},{"l":"com.mineinjava.quail.localization"},{"l":"com.mineinjava.quail.pathing"},{"l":"com.mineinjava.quail.util"},{"l":"com.mineinjava.quail.util.geometry"}];updateSearchResults();
