24edaa7d211771ab4e990f32b373239b7175b30c
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2024-01-25 11:11:48 -0800

    implements a class for accleration limited vectors
    not currently tested
    
    Co-authored-by: Hudson Hilal <112907326+hhilal123@users.noreply.github.com>

---
+++ quail/src/main/java/com/mineinjava/quail/util/geometry/AccelerationLimitedVector.java
@@ -0,0 +1,69 @@
+package com.mineinjava.quail.util.geometry;
+
+/**
+ * Represents a vector with input ramping to control acceleration
+ * Useful for joystick ramp limiting
+ */
+class AccelerationLimitedVector{
+    Vec2d idealVector;
+    Vec2d currentVector;
+    double maxAcceleration;
+    double looptime;
+    double lastTime = 0;
+    /**
+     * Creates a new AccelerationLimitedVector
+     * @param idealVector The vector to ramp to
+     * @param currentVector The current vector
+     * @param maxAcceleration The maximum acceleration
+     */
+    public AccelerationLimitedVector(Vec2d idealVector, Vec2d currentVector, double maxAcceleration) {
+        this.idealVector = idealVector;
+        this.currentVector = currentVector;
+        this.maxAcceleration = maxAcceleration;
+    }
+    /**
+     * Creates a new AccelerationLimitedVector
+     * @param maxAcceleration The maximum acceleration
+     */
+    public AccelerationLimitedVector(double maxAcceleration) {
+        this.idealVector = new Vec2d(0,0);
+        this.currentVector = new Vec2d(0,0);
+        this.maxAcceleration = maxAcceleration;
+    }
+
+    /**
+     * Gets the ideal vector
+     * @return current ideal vector
+     */
+    public Vec2d getIdealVector(){
+        return idealVector;
+    }
+
+    /**
+     * calculates a new output vector based on the time since the last update
+     * @return
+     */
+    public Vec2d update(){
+        if (this.lastTime == 0){
+            this.lastTime = System.currentTimeMillis();
+        }
+        this.looptime = (System.currentTimeMillis() - this.lastTime)/1000.0;
+
+        Vec2d accelerationVector = idealVector.subtract(currentVector).scale(1/looptime);
+        if(accelerationVector.getLength() > maxAcceleration){
+            accelerationVector = accelerationVector.normalize().scale(maxAcceleration);
+        }
+        currentVector = currentVector.add(accelerationVector.normalize().scale(looptime));
+        return currentVector;
+    }
+
+    /**
+     * updates the ideal vector and calculates a new output vector based on the time since the last update
+     * @param newIdealVector The new ideal vector
+     * @return
+     */
+    public Vec2d update(Vec2d newIdealVector) {
+        this.idealVector = newIdealVector;
+        return update();
+    }
+}
