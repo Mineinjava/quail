98e95502a7df78f42061c45babcfb79ff27c998f
Bernie Conrad <root@bernstern.dev>
2024-02-23 12:34:49 -0800

    Flushed out angle class to make angle ops safer

--- quail/src/main/java/com/mineinjava/quail/util/geometry/Angle.java
+++ quail/src/main/java/com/mineinjava/quail/util/geometry/Angle.java
@@ -1,14 +1,94 @@
+/*
+ * Class to represent an ccw+ angle in radians and provide various utilities for working with angles in radians or degrees.
+ * 
+ * The purpose is to provide a more intuitive and safe way to work with angles in radians and degrees.
+ */
+
 package com.mineinjava.quail.util.geometry;
 
 /** Various utilities for working with angles. ALL angles MUST be in ccw+ radians */
 public class Angle {
   private static final double TAU = 2 * Math.PI;
 
+  private final double radians;
+
+  /**
+   * Constructs a new {@link Angle} with the given measure in radians.
+   * @param radians
+   */
+  private Angle(double radians) {
+    this.radians = radians;
+  }
+
+  /**
+   * Factory method to create a new {@link Angle} with the given measure in radians.
+   * @param radians measure in radians (ccw+)
+   * @return a new {@link Angle} with the given measure in radians
+   */
+  public static Angle fromRadians(double radians) {
+    return new Angle(radians);
+  }
+
+  /**
+   * Factory method to create a new {@link Angle} with the given measure in degrees.
+   * @param degrees measure in degrees (ccw+)
+   * @return a new {@link Angle} with the given measure in degrees
+   */
+  public static Angle fromDegrees(double degrees) {
+    return new Angle(Math.toRadians(degrees));
+  }
+
+  /**
+   * Returns the measure of this angle in degrees.
+   * @return the measure of this angle in degrees
+   */
+  public double getDegrees() {
+    return Math.toDegrees(radians);
+  }
+
+  /**
+   * Returns the measure of this angle in radians.
+   * @return the measure of this angle in radians
+   */
+  public double getRadians() {
+    return radians;
+  }
+
+  /**
+   * Add {@code angle} to this angle and return the result.
+   * @param angle angle to add
+   * @return the result of adding {@code angle} to this angle
+   */
+  public Angle add(Angle angle) {
+    return new Angle(radians + angle.radians);
+  }
+
+  /**
+   * Subtract {@code angle} from this angle and return the result.
+   *
+   * @param angle angle to subtract
+   * @return the result of subtracting {@code angle} from this angle
+   */
+  public Angle subtract(Angle angle) {
+    return new Angle(radians - angle.radians);
+  }
+
+  /**
+   * Returns this {@code Angle} clamped to {@code [0, 2pi]}.
+   *
+   * @param angle angle measure in radians
+   */
+  public Angle norm() {
+    double modifiedAngle = ((radians % TAU) + TAU) % TAU;
+    return new Angle(modifiedAngle);
+  }
+
   /**
    * Returns {@code angle} clamped to {@code [0, 2pi]}.
    *
    * @param angle angle measure in radians
    */
+  @Deprecated
   public static double norm(double angle) {
     double modifiedAngle = angle % TAU;
 
@@ -18,10 +98,26 @@ public class Angle {
   }
 
   /**
+   * Returns this {@code Angle} clamped to {@code [-pi, pi]}.
+   *
+   * @param angleDelta angle delta in radians
+   */
+  public Angle normDelta() {
+    double modifiedAngleDelta = norm().radians;
+
+    if (modifiedAngleDelta > Math.PI) {
+      modifiedAngleDelta -= TAU;
+    }
+
+    return new Angle(modifiedAngleDelta);
+  }
+
+  /**
    * Returns {@code angleDelta} clamped to {@code [-pi, pi]}.
    *
    * @param angleDelta angle delta in radians
    */
+  @Deprecated
   public static double normDelta(double angleDelta) {
     double modifiedAngleDelta = norm(angleDelta);
 
@@ -30,5 +126,25 @@ public class Angle {
     }
 
     return modifiedAngleDelta;
+  }
+
+  @Override
+  public String toString() {
+    return "Angle{radians=" + radians + "}";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof Angle)) {
+      return false;
+    }
+
+    Angle angle = (Angle) o;
+
+    return Double.compare(angle.radians, radians) == 0;
   }
 }
---
+++ quail/src/test/java/quail/util/geometry/AngleTest.java
@@ -0,0 +1,92 @@
+// Tests for the Angle class
+
+package quail.util.geometry;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.mineinjava.quail.util.geometry.Angle;
+
+class AngleTest {
+
+  @Test
+  void angleConstructorsAndGetters() {
+    Angle a = Angle.fromRadians(0);
+    assertEquals(0, a.getRadians());
+    assertEquals(0, a.getDegrees());
+
+    a = Angle.fromRadians(Math.PI);
+    assertEquals(Math.PI, a.getRadians());
+    assertEquals(180, a.getDegrees());
+
+    a = Angle.fromDegrees(0);
+    assertEquals(0, a.getRadians());
+    assertEquals(0, a.getDegrees());
+
+    a = Angle.fromDegrees(180);
+    assertEquals(Math.PI, a.getRadians());
+    assertEquals(180, a.getDegrees());
+  }
+
+  @Test 
+  void angleAddition() {
+    Angle a = Angle.fromRadians(0);
+    Angle b = Angle.fromRadians(Math.PI);
+    Angle c = a.add(b);
+    assertEquals(Math.PI, c.getRadians());
+
+    a = Angle.fromDegrees(0);
+    b = Angle.fromDegrees(180);
+    c = a.add(b);
+    assertEquals(Math.PI, c.getRadians());
+
+    a = Angle.fromRadians(Math.PI);
+    a = Angle.fromDegrees(-180);
+    c = a.add(b);
+    assertEquals(0, c.getRadians());
+  }
+
+  @Test
+  void angleSubtraction() {
+    Angle a = Angle.fromRadians(0);
+    Angle b = Angle.fromRadians(Math.PI);
+    Angle c = a.subtract(b);
+    assertEquals(-Math.PI, c.getRadians());
+
+    a = Angle.fromDegrees(0);
+    b = Angle.fromDegrees(180);
+    c = a.subtract(b);
+    assertEquals(-Math.PI, c.getRadians());
+  }
+
+  @Test
+  void angleNormalization() {
+    Angle a = Angle.fromDegrees(800);
+    assertEquals(Angle.norm(a.getRadians()), a.norm().getRadians());
+
+    a = Angle.fromDegrees(-800);
+    assertEquals(Angle.norm(a.getRadians()), a.norm().getRadians());
+  }
+
+
+  @Test
+  void angleNormalizationDelta() {
+    Angle a = Angle.fromDegrees(800);
+    assertEquals(Angle.normDelta(a.getRadians()), a.normDelta().getRadians(), 0.0001);
+
+    a = Angle.fromDegrees(-800);
+    assertEquals(Angle.normDelta(a.getRadians()), a.normDelta().getRadians(), 0.0001);
+  }
+
+  @Test
+  void angleEquality() {
+    Angle a = Angle.fromDegrees(0);
+    Angle b = Angle.fromDegrees(0);
+    assertEquals(a, b);
+
+    a = Angle.fromDegrees(0);
+    b = Angle.fromDegrees(180);
+    assertNotEquals(a, b);
+  }
+}
