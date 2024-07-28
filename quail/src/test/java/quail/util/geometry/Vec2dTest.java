914094ce8ca6831f6a125e47b295209fc28c2b4d
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2024-07-28 00:21:43 -0700

    Finish testing quail.util.geometry.Vec2d
    It passes!

--- quail/src/test/java/quail/util/geometry/Vec2dTest.java
+++ quail/src/test/java/quail/util/geometry/Vec2dTest.java
@@ -104,9 +104,9 @@ public class Vec2dTest {
     Vec2d vec2 = new Vec2d(0, 1);
     Vec2d vec3 = new Vec2d(0, -1);
 
-    assertEquals(0, vec1.getAngle());
-    assertEquals(Math.PI / 2, vec2.getAngle());
-    assertEquals(Math.PI * 3 / 2, vec3.getAngle());
+    assertEquals(0, vec1.getAngle(), "<0,1> has 0 angle");
+    assertEquals(Math.PI / 2, vec2.getAngle(), "<0,1> has pi/2 angle");
+    assertEquals(Math.PI * 3 / 2, vec3.getAngle(), "<0, -1> has 3pi/2 angle");
   }
 
   @Test
@@ -117,14 +117,62 @@ public class Vec2dTest {
     Vec2d vec4 = new Vec2d(-1, 0);
     Vec2d vec5 = new Vec2d(0, -1);
 
-    assertEquals(Double.NaN, vec1.distanceTo(vec1));
-    assertEquals(Double.NaN, vec1.distanceTo(vec2));
+    assertEquals(Double.NaN, vec1.distanceTo(vec1), "Angular distance between zero vectors is NaN");
+    assertEquals(
+        Double.NaN, vec1.distanceTo(vec2), "Angular distance involving zero vector is NaN");
 
-    assertEquals(Math.PI/2, vec2.distanceTo(vec3));
-    assertEquals(Math.PI/2, vec5.distanceTo(vec4));
-    assertEquals(Math.PI/2, vec3.distanceTo(vec4));
+    assertEquals(
+        Math.PI / 2, vec2.distanceTo(vec3), "Angular distance of a cw perpendicular is pi/2");
+    assertEquals(
+        Math.PI / 2, vec5.distanceTo(vec4), "Angular distance of a ccw perpendicular is pi/2");
+    assertEquals(
+        Math.PI / 2,
+        vec3.distanceTo(vec4),
+        "Angular distance of a cw perpendicular when y<0 is pi/2");
 
     assertEquals(Math.PI, vec2.distanceTo(vec4));
   }
 
+  @Test
+  public void testDotProduct() {
+    Vec2d v = new Vec2d(3, 4);
+    Vec2d vPerp = new Vec2d(-4, 3);
+    Vec2d vZero = new Vec2d(0, 0);
+    Vec2d vNeg = new Vec2d(-1, -1);
+
+    // Dot product with the same vector
+    double result = v.dot(v);
+    assertEquals(25, result, "Dot product of the same vector should be the squared length");
+
+    // Dot product with a perpendicular vector
+    result = v.dot(vPerp);
+    assertEquals(0, result, "Dot product of perpendicular vectors should be zero");
+
+    // Dot product with a zero vector
+    result = v.dot(vZero);
+    assertEquals(0, result, "Dot product with zero vector should be zero");
+
+    // Dot product with negative coordinates
+    result = v.dot(vNeg);
+    assertEquals(-7, result, "Dot product with negative coordinates should match expected value");
+  }
+
+  @Test
+  public void testCrossProduct() {
+    Vec2d v = new Vec2d(3, 4);
+    Vec2d vPerp = new Vec2d(-4, 3);
+    Vec2d vZero = new Vec2d(0, 0);
+
+    // Cross product with the same vector
+    double result = v.cross(v);
+    assertEquals(0, result, "Cross product of the same vector should be zero");
+
+    // Cross product with a perpendicular vector
+    result = v.cross(vPerp);
+    assertEquals(25, result, "Cross product of perpendicular vectors should be the squared length");
+
+    // Cross product with a zero vector
+    result = v.cross(vZero);
+    assertTrue(MathUtil.epsilonEquals(0, result), "Cross product with zero vector should be zero");
+  }
 }
